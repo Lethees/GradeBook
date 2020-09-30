d8193f5caef1fa4fba334081f4d7290a3b0f29b0
lethees <514180089@qq.com>
2020-09-30 16:51:24 -0600

    Initial commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name){
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+        public Statistic GetStatistics()
+        {
+            var result = new Statistic();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach(var grade in grades) {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+            }
+
+            result.Average = result.Average / grades.Count;
+
+            return result;
+        }
+        public List<double> grades;
+
+        private string name;
+    }
+}
